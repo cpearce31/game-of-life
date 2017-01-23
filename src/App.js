69bb1537e101750baca6001093660b7434b57241
Caleb Pearce <cpearce31@gmail.com>
2017-01-23 17:03:45 -0500

    Inline styling works properly, displays a basic grid.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,62 @@
 import React, { Component } from 'react';
 import './App.css';
+import Cell from './Cell.js';
+
+function generateRandomBoard (height, width) {
+  let board = [];
+  for (let i = 0; i < height; i++) {
+    let row = [];
+    for (let j = 0; j < width; j++) {
+      row.push(Math.round(Math.random()));
+    }
+    board.push(row);
+  }
+  return board;
+}
+
+function buildBoard (board, cellSize) {
+  let cells = [];
+  for (let y = 0; y < board.length; y++) {
+    for (let x = 0; x < board[y].length; x++) {
+      let alive = board[y][x] === 1;
+      let cellStyle = {
+        position: 'absolute',
+        height: cellSize,
+        width: cellSize,
+        top: y * cellSize,
+        left: x * cellSize
+      };
+      cells.push(<Cell
+        row={y}
+        col={x}
+        alive={alive}
+        size={cellSize}
+        style={cellStyle}
+        className='cell'
+        key={'y' + y + 'x' + x}
+      />);
+    }
+  }
+  return cells;
+}
 
 class App extends Component {
-  render() {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      width: 100,
+      height: 80,
+      paused: false,
+      cellSize: 20,
+      board: generateRandomBoard(4, 5)
+    };
+  }
+
+  render () {
     return (
-      <div className="App">
+      <div>
+        {buildBoard(this.state.board, this.state.cellSize)}
       </div>
     );
   }
---
+++ src/Cell.js
@@ -0,0 +1,11 @@
+const React = require('react');
+
+class Cell extends React.Component {
+  render () {
+    console.log(this.props.style);
+    let test = this.props.alive ? '#' : '.';
+    return <span className='cell-inner' style={this.props.style}>{test}</span>;
+  }
+}
+
+module.exports = Cell;
