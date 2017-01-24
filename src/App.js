009b04ed97497268fe08d854ffe10fb86cf9a820
Caleb Pearce <cpearce31@gmail.com>
2017-01-24 10:27:11 -0500

    Tick function is working but render is totally broken.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import Cell from './Cell.js';
 
+let cells = [];
+
 function generateRandomBoard (height, width) {
   let board = [];
   for (let i = 0; i < height; i++) {
@@ -14,36 +16,38 @@ function generateRandomBoard (height, width) {
   return board;
 }
 
-function buildBoard (board, cellSize) {
-  let cells = [];
-  for (let y = 0; y < board.length; y++) {
-    for (let x = 0; x < board[y].length; x++) {
-      let alive = board[y][x] === 1;
-      let cellStyle = {
-        position: 'absolute',
-        height: cellSize,
-        width: cellSize,
-        top: y * cellSize,
-        left: x * cellSize
-      };
-      cells.push(<Cell
-        row={y}
-        col={x}
-        alive={alive}
-        size={cellSize}
-        style={cellStyle}
-        className='cell'
-        key={'y' + y + 'x' + x}
-      />);
-    }
+function countLiveNeighbors (y, x, board) {
+  function getAdjacentCoords (y, x, board) {
+    const possibleCoords = [
+      [y - 1, x - 1],
+      [y, x - 1],
+      [y + 1, x - 1],
+      [y - 1, x],
+      [y + 1, x],
+      [y - 1, x + 1],
+      [y, x + 1],
+      [y + 1, x + 1]
+    ];
+    return possibleCoords.filter(
+      (coord) => coord[0] >= 0 &&
+                 coord[1] >= 0 &&
+                 coord[0] < board.length &&
+                 coord[1] < board.length
+    );
   }
-  return cells;
+  let coords = getAdjacentCoords(y, x, board);
+  let count = 0;
+  for (let i = 0; i < coords.length; i++) {
+    count += board[coords[i][0]][coords[i][1]];
+  }
+  return count;
 }
 
 class App extends Component {
 
   constructor (props) {
     super(props);
+
     this.state = {
       width: 20,
       height: 20,
@@ -52,47 +56,76 @@ class App extends Component {
       board: generateRandomBoard(20, 20),
       cells: []
     };
+
+    this.tick = this.tick.bind(this);
   }
 
   componentWillMount () {
-    this.setState({
-      cells: buildBoard(this.state.board, this.state.cellSize)
-    });
-  }
-
-  tick () {
-    let cells = this.state.cells;
-    let offset = this.state.width;
-    for (let i = 0; i < cells.length; i++) {
-      let count = 0;
-      let neighbors = [
-        cells[i - offset - 1],
-        cells[i - offset],
-        cells[i - offset + 1],
-        cells[i - 1],
-        cells[i + i],
-        cells[i + offset - 1],
-        cells[i + offset],
-        cells[i + offset + 1]
-      ];
-      for (let j = 0; j < neighbors.length; j++) {
-        if (neighbors[i].props.alive) {
-          count++;
-        }
+    for (let y = 0; y < this.state.board.length; y++) {
+      for (let x = 0; x < this.state.board[y].length; x++) {
+        let cellStyle = {
+          position: 'absolute',
+          height: this.state.cellSize,
+          width: this.state.cellSize,
+          top: y * this.state.cellSize,
+          left: x * this.state.cellSize
+        };
+        cells.push(<Cell
+          row={y}
+          col={x}
+          alive={this.state.board[y][x] === 1} // May cause a bug if y/x dont persist
+          size={this.state.cellSize}
+          style={cellStyle}
+          className='cell'
+          key={'y' + y + 'x' + x}
+        />);
       }
     }
   }
 
-  render () {
-    return (
-      <div>
-        {this.state.cells}
-      </div>
-    );
+  tick () {
+    console.log('tick fired. state: ');
+    console.log(this.state);
+    let newBoard = JSON.parse(JSON.stringify(this.state.board));
+    for (let y = 0; y < this.state.height; y++) {
+      for (let x = 0; x < this.state.width; x++) {
+        let n = countLiveNeighbors(y, x, this.state.board);
+        let c = this.state.board[y][x];
+        // console.log('c: ' + c + ' n: ' + n);
+        if (n < 2 && c === 1) {
+          newBoard[y][x] = 0;
+        } else if (n > 3 && c === 1) {
+          newBoard[y][x] = 0;
+        } else if (c === 1) {
+          newBoard[y][x] = 1;
+        } else if (n === 3 && c === 0) {
+          newBoard[y][x] = 1;
+        }
+      }
+    }
+    console.log(newBoard);
+    this.setState({
+      board: newBoard
+    });
   }
 
   componentDidMount () {
-    console.log(this.props);
+    const t = setTimeout(this.tick, 1000);
+  }
+
+  render () {
+    for (let i = 0; i < cells.length; i++) {
+      let y = cells[i].props.row;
+      let x = cells[i].props.col;
+      cells[i] = <Cell alive={this.state.board[y][x] !== 0} />;
+    }
+    console.log('state inside reder:');
+    console.log(this.state);
+    return (
+      <div>
+        {cells}
+      </div>
+    );
   }
 }
 
--- src/Cell.js
+++ src/Cell.js
@@ -2,6 +2,7 @@ const React = require('react');
 
 class Cell extends React.Component {
   render () {
+    console.log(this.props);
     let test = this.props.alive ? '#' : '.';
     return <span className='cell-inner' style={this.props.style}>{test}</span>;
   }
